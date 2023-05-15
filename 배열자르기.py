ae8e61cd100db3c5cdb7aa1cb9927087c7b3d32e
kimyeonjun0307 <magic201358@naver.com>
2023-05-15 15:48:03 +0900

    new

---
+++ 배열자르기.py
@@ -0,0 +1,5 @@
+def solution(numbers, num1, num2): 
+    for i in numbers:
+        i = numbers[num1:num2+1]   # 배열[2:3] 배열의2번째에서 3번째까지 위치정보
+                                   # num2 +1 이유
+    return i
